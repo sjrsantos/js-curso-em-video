62b8002988f9ed8de741c518a43dd1ecf949b8fc
Silvio Júnior <silvio-sjunior@hotmail.com>
2021-03-24 20:15:43 -0300

    finaliza 11 aula e exercícios

---
+++ Aula 11/ex012.js
@@ -0,0 +1,6 @@
+var vel = 57.8
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 50) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
